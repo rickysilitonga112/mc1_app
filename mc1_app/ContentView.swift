ae491208dd062956aee07187df2e9f483b26bdfa
“rickysilitonga112” <rickysilitonga112@gmail.com>
2022-05-17 19:33:46 +0700

    Initial Commit

---
+++ mc1_app/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  mc1_app
+//
+//  Created by Ricky Silitonga on 17/05/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ mc1_app/mc1_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  mc1_appApp.swift
+//  mc1_app
+//
+//  Created by Ricky Silitonga on 17/05/22.
+//
+
+import SwiftUI
+
+@main
+struct mc1_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
